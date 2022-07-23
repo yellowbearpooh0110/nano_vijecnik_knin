b457ce16e71769dd278a1dea08c1c7df9848044d
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-07-23 23:11:20 +0900

    event emit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Route::get('/t', function () {
+    event(new \App\Events\MessageCreated());
+    dd('Event Run Successfully.');
+});
