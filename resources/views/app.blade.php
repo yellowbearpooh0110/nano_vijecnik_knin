a2fc6974dea323ac4b649350337fa88e2e4830a0
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-07-25 21:35:08 +0900

    add csrf meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel 9 with vue 3 vite</title>
-
 </head>
 
 <body>
