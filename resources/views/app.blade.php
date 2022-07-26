b72777fc260525dc446e2260a1311ebb91fed6ac
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-07-26 12:09:50 +0900

    implement shouldboradcastnow

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,11 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel 9 with vue 3 vite</title>
+    <title>Laravel Broadcast Redis Socket io Tutorial - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.css" />
 </head>
 
 <body>
